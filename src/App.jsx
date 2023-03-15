dcb46301b3f581753cfa3532ebddb0a2ac641864
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-15 14:01:22 +0800

    deleted app contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { Sidebar, Table } from "./components"
-import {BrowserRouter as Router} from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <Router>
-        <Sidebar />
-      </Router>
     </>
   )
 }
