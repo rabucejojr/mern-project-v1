49e45685e54642cd08de7e3ac211de2b5ea5b20f
Roger Abucejo <rogerabucejo62@gmail.com>
2023-03-17 17:30:55 +0800

    setup ui for basic crud layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { Card, Grid, CardContent, TextField } from '@mui/material'
+import { Card, Grid, CardContent, TextField, Button } from '@mui/material'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Card>
-        <CardContent>
-          <Grid item >
+      <Card >
+        <CardContent container spacing={2}>
+          <Grid  item >
             <TextField label='First Name' variant='outlined'></TextField>
             <TextField label='Last Name' variant='outlined'></TextField>
+            <Button variant='contained'>Send</Button>
           </Grid>
         </CardContent>
       </Card>
