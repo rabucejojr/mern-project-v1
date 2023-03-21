971cb5e886938abec1e030fd073957139e917982
rabucejo <rogerhapay@gmail.com>
2023-03-21 09:04:23 +0800

    created dummy route for testing

--- server/server.js
+++ server/server.js
@@ -3,15 +3,28 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 3001
 const cors = require('cors')
 const mongoose = require('mongoose')
+const routes = require('./routes')
 
 const app = express()
-mongoose.connect('mongodb://localhost:27017/mydatabase')
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/mydatabase',{useNewUrlParser:true, useUnifiedTopology:true});
+const connection = mongoose.connection;
+
+connection.once('open',()=>{console.log('MongoDB connected!')});
+
+app.listen(port,()=>{console.log(`Server running in port ${port}`)})
+
+app.use('/', routes);
 
 // FOR CHECKING MONGODB CONNECTION
-mongoose.connection.on('open', () => {
-  console.log('MongoDB connected!')
-})
+// mongoose.connection.on('open', () => {
+//   console.log('MongoDB connected!')
+// })
 
-mongoose.connection.on('error', (err) => {
-  console.log(`MongoDB connection error: ${err}`)
-})
+// mongoose.connection.on('error', (err) => {
+//   console.log(`MongoDB connection error: ${err}`)
+// })
+
+
