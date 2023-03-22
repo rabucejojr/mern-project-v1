e8151cd4882594102f6e3efa7d93fb92758733d4
rabucejo <rogerhapay@gmail.com>
2023-03-22 13:30:15 +0800

    redesigned a new sidebar

--- client/src/Components/Dashboard/index.jsx
+++ client/src/Components/Dashboard/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import {Grid,Typography,Paper} from '@mui/material'
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Paper sx={{ height: '100vh', p:2 }}>
+            <Typography variant='h6'>Widget 1</Typography>
+            {/* <Typography variant='body1'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
+              amet elit ut nibh varius tempus. Ut euismod sapien sed dui
+              facilisis, a fermentum eros bibendum. Suspendisse vitae dui
+              euismod, hendrerit nisl vitae, malesuada metus. Nam consequat
+              velit in nibh fringilla molestie.
+            </Typography> */}
+          </Paper>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
