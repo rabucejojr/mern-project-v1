0c7e4f97c639dfb042d3b3a2db7887a42b3a7d75
rabucejo <rogerhapay@gmail.com>
2023-03-27 19:56:22 +0800

    finishing touches; css styles

--- client/src/components/DocumentDetails.jsx
+++ client/src/components/DocumentDetails.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 //hooks
 import { useDocumentsContext } from '../hooks/useDocumentContext'
 export const DocumentDetails = ({ document }) => {
-  const {dispatch} = useDocumentsContext()
+  const { dispatch } = useDocumentsContext()
   const handleClick = async () => {
     try {
-          
-    const response = await fetch(
-      'http://localhost:5000/api/documents/' + document._id,
-      {
-        method: 'DELETE',
-      }
-    )
-    const json = await response.json()
-    dispatch({typee:'DELETE_DOCUMENT',payload:json})
+      const response = await fetch(
+        'http://localhost:5000/api/documents/' + document._id,
+        {
+          method: 'DELETE',
+        }
+      )
+      const json = await response.json()
+      dispatch({ typee: 'DELETE_DOCUMENT', payload: json })
     } catch (error) {
       console.log(error)
     }
@@ -30,7 +29,7 @@ export const DocumentDetails = ({ document }) => {
         {document.desc}
       </p>
       <p>{document.createdAt}</p>
-      <span onClick={handleClick}>delete</span>
+      <span onClick={handleClick}>Delete</span>
     </div>
   )
 }
