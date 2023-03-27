46ac9412cacd9a103982106ab582c6bc43d73595
rabucejo <rogerhapay@gmail.com>
2023-03-27 16:59:24 +0800

    fix error handlings

--- server/controllers/documentController.js
+++ server/controllers/documentController.js
@@ -1,6 +1,7 @@
 const Document = require('../models/documentModel')
 const mongoose = require('mongoose')
-//get all documents
+
+//GET ALL DOCUMENTS
 const getDocuments = async (req, res) => {
   const documents = await Document.find({}).sort({ createAt: -1 })
   res.status(200).json(documents)
@@ -13,7 +14,6 @@ const getDocument = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({ error: 'Invalid id provided' })
   }
-
   const document = await Document.findById(id)
   if (!document) {
     return res.status(404).json({ error: 'Document not found' })
@@ -22,10 +22,29 @@ const getDocument = async (req, res) => {
 }
 // CREATE/UPLOAD NEW DOCUMENT
 const uploadDocument = async (req, res) => {
-  const { filename, path, desc,date } = req.body
+  const { filename, path, desc, date } = req.body
+
+  let emptyFields = []
+
+  //CHECK FOR EMPTY FIELDS
+  if (!filename) {
+    emptyFields.push('filename')
+  }
+  if (!path) {
+    emptyFields.push('path')
+  }
+  if (!desc) {
+    emptyFields.push('desc')
+  }
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: 'PLease fill in all fields' }, emptyFields)
+  }
+
   //add document to db
   try {
-    const document = await Document.create({ filename, path, desc,date })
+    const document = await Document.create({ filename, path, desc, date })
     res.status(200).json(document)
   } catch (error) {
     res.status(400).json({ error: error.message })
