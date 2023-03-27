da7c687f28252f0b3100197e4246530a20bbe3a5
rabucejo <rogerhapay@gmail.com>
2023-03-27 12:47:05 +0800

    added documentContext provider and hook

---
+++ client/src/context/DocumentContext.jsx
@@ -0,0 +1,32 @@
+import { React, createContext, useReducer } from 'react'
+
+export const DocumentContext = createContext()
+export const documentsReducer = (state, action)=>{
+    switch(action.type){
+        case 'SET_DOCUMENTS':
+            return {
+                documents: action.payload
+            }
+        case 'CREATE_DOCUMENT':
+            return{
+                documents:[action.payload,...state.documents]
+            }
+        default:
+            return state
+    }
+}
+
+export const DocumentContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(documentsReducer,{
+    documents:null,
+  })
+//   dispatch({type:''})
+
+  return (
+    <>
+      <DocumentContext.Provider value={{ state, dispatch }}>
+        {children}
+      </DocumentContext.Provider>
+    </>
+  )
+}
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { DocumentContextProvider } from './context/DocumentContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <DocumentContextProvider>
+      <App />
+    </DocumentContextProvider>
+  </React.StrictMode>
 )
